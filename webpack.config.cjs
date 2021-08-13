019168ffaaf11d468353ee12ea32a797ba9db496
warpedrhubarb <haveanicedayy420@gmail.com>
2021-08-13 17:35:38 +0300

    :rocket: create html template, add webpack and bootstrap, deploy

---
+++ webpack.config.cjs
@@ -0,0 +1,18 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'RSS Generator',
+      template: './index.html',
+    }),
+  ],
+};
